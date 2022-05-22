d93ccc5892fde89065859e6f58389a5be52f5bde
monge <alberto.monge@polito.it>
2022-05-22 19:08:27 +0200

    Progetto base

---
+++ src/main/java/it/polito/tdp/crimes/FXMLController.java
@@ -0,0 +1,69 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.crimes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.crimes.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="boxCategoria"
+    private ComboBox<?> boxCategoria; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxMese"
+    private ComboBox<?> boxMese; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnAnalisi"
+    private Button btnAnalisi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxArco"
+    private ComboBox<?> boxArco; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnPercorso"
+    private Button btnPercorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doCalcolaPercorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert boxCategoria != null : "fx:id=\"boxCategoria\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert boxMese != null : "fx:id=\"boxMese\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAnalisi != null : "fx:id=\"btnAnalisi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert boxArco != null : "fx:id=\"boxArco\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnPercorso != null : "fx:id=\"btnPercorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }
+}
---
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -0,0 +1,6 @@
+package it.polito.tdp.crimes.model;
+
+
+public class Model {
+	
+}
